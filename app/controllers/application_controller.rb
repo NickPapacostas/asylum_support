13e5f629e9082c3a96744e052883c35e28d60f56
NickPapacostas <nicholas@heetch.com>
2019-05-03 21:32:37 +0300

    adding devise custom views for name on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_caseworker!
-
+ 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(caseworker)
   	caseworker_dashboard_path(caseworker)
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
 	def after_sign_out_path_for(caseworker)
 	  new_session_path(caseworker)
 	end
+
+  protected
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
