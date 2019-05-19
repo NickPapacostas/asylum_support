fde6f14bc4fee625f802ea6fc637aee7421823af
NickPapacostas <nicholas@heetch.com>
2019-05-19 23:11:02 +0300

    removing duplicate geographic restriction in migration

--- db/migrate/20190519172004_add_interview_date_asylum_office_to_member.rb
+++ db/migrate/20190519172004_add_interview_date_asylum_office_to_member.rb
@@ -5,7 +5,6 @@ class AddInterviewDateAsylumOfficeToMember < ActiveRecord::Migration[5.2]
   	add_column :members, :amka, :boolean
   	add_column :members, :afm, :boolean
   	add_column :members, :bank_account, :boolean
-  	add_column :members, :geographic_restriction, :boolean
   	change_column :members, :arrival_date, :date
   end
 end
